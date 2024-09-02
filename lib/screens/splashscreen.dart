170866dc2ae3ecc81aeeec2bb8f1af9ad9b54969
Faris Afra <diafan74@gmail.com>
2024-09-02 08:20:11 +0700

    Update: Splash Screen and Logo

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:paket_tracker_app/screens/homepage.dart';
 import 'package:paket_tracker_app/screens/widgets/colors.dart';
@@ -9,49 +10,17 @@ class Splashscreen extends StatefulWidget {
   _SplashscreenState createState() => _SplashscreenState();
 }
 
-class _SplashscreenState extends State<Splashscreen> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _logoSizeAnimation;
-  late bool _showLogo1;
-
+class _SplashscreenState extends State<Splashscreen> {
   @override
   void initState() {
     super.initState();
-    _showLogo1 = true;
 
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 3), // Total duration is 3 seconds for one logo
-      vsync: this,
-    );
-
-    _logoSizeAnimation = Tween<double>(begin: 0, end: 157).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: const Interval(0.0, 0.166, curve: Curves.easeIn), // 0 to 0.5 seconds
-        reverseCurve: const Interval(0.833, 1.0, curve: Curves.easeOut), // 2.5 to 3 seconds
-      ),
-    )..addListener(() {
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          _animationController.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          if (_showLogo1) {
-            setState(() {
-              _showLogo1 = false;
-            });
-            _animationController.forward();
-          } else {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => Homepage()),
-            );
-          }
-        }
-      });
-
-    _animationController.forward();
+    Timer(const Duration(milliseconds: 3000), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Homepage()),
+      );
+    });
   }
 
   @override
@@ -60,16 +29,10 @@ class _SplashscreenState extends State<Splashscreen> with TickerProviderStateMix
       backgroundColor: AppColors.BiruPrimary,
       body: Center(
         child: Image.asset(
-          _showLogo1 ? 'assets/images/logo_app_1.png' : 'assets/images/logo_app_2.png',
-          height: _logoSizeAnimation.value,
+          'assets/images/SplashScreen.gif',
+          fit: BoxFit.contain,
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
   }
 }
